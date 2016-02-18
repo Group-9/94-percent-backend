cfc021150dc159ce25bf6cf913779290846f3037
Eoin Houlihan <ehoulih@tcd.ie>
2016-02-18 11:45:57 +0000

    Enable CORS on all routes

--- app.rb
+++ app.rb
@@ -1,11 +1,16 @@
 require 'sinatra'
 require 'sinatra/activerecord'
+require 'sinatra/cross_origin'
 
 Dir[File.join('models', '**/*.rb')].each do |f|
   require_relative f
 end
 
 set :port, 3000
+
+configure do
+  enable :cross_origin
+end
 
 get '/' do
   'Hello world'
