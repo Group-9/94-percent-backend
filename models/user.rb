45623eb94ec26cbd30f17d3e408fec463d7d2dff
Eoin Houlihan <ehoulih@tcd.ie>
2016-03-24 15:02:29 +0000

    Fix user model

--- models/user.rb
+++ models/user.rb
@@ -1,12 +1,12 @@
 class User < ActiveRecord::Base
 	enum gender: [:male, :female, :other]
 	validates :gender, presence: true, numericality: {greater_than:-1, less_than:3}
-	
+
 	enum education: [:uneducated, :primary, :secondary, :diploma, :bachelors, :masters, :docotorate]
 	validates :education, presence: true, numericality: {greater_than:-1, less_than:7}
-	
+
 	enum employment_status: [:student, :employed, :unemployed]
-	validates :employment, presence: true, numericality: {greater_than:-1, less_than:3}
-	
+  validates :employment_status, presence: true, numericality: {greater_than:-1, less_than:3}
+
 	has_many :entries
 end
