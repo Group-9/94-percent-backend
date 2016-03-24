6a61609253eb27a7e78ccdde41c01740ac3ea81d
Patrick Meleady <Patrick Meleady>
2016-03-24 13:02:00 +0000

    Updated user model to fit requirements

--- models/user.rb
+++ models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
-  has_many :entries
+	enum gender: [:male, :female, :other]
+	validates :gender, presence: true, numericality: {greater_than:-1, less_than:3}
+	
+	enum education: [:uneducated, :primary, :secondary, :diploma, :bachelors, :masters, :docotorate]
+	validates :education, presence: true, numericality: {greater_than:-1, less_than:7}
+	
+	enum employment_status: [:student, :employed, :unemployed]
+	validates :employment, presence: true, numericality: {greater_than:-1, less_than:3}
+	
+	has_many :entries
 end
