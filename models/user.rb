dfd230e1d15fac08ba53f3330133dea2d9262386
Eoin Houlihan <ehoulih@tcd.ie>
2016-03-24 16:12:29 +0000

    Don't use numericality validations

--- models/user.rb
+++ models/user.rb
@@ -1,12 +1,9 @@
 class User < ActiveRecord::Base
-	enum gender: [:male, :female, :other]
-	validates :gender, presence: true, numericality: {greater_than:-1, less_than:3}
+  enum gender: [:male, :female, :other]
 
-	enum education: [:uneducated, :primary, :secondary, :diploma, :bachelors, :masters, :docotorate]
-	validates :education, presence: true, numericality: {greater_than:-1, less_than:7}
+  enum education: [:uneducated, :primary, :secondary, :diploma, :bachelors, :masters, :doctorate]
 
-	enum employment_status: [:student, :employed, :unemployed]
-  validates :employment_status, presence: true, numericality: {greater_than:-1, less_than:3}
+  enum employment_status: [:student, :employed, :unemployed]
 
-	has_many :entries
+  has_many :entries
 end
