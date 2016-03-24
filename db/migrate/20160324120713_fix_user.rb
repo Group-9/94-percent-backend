9e27c09adf74b208916f0c407fd98c2d8fb668ea
Eoin Houlihan <ehoulih@tcd.ie>
2016-03-24 12:40:16 +0000

    Users

--- db/migrate/20160324120713_fix_user.rb
+++ db/migrate/20160324120713_fix_user.rb
@@ -2,7 +2,8 @@ class FixUser < ActiveRecord::Migration
   def change
     remove_column :users, :gender
     add_column :users, :gender, :integer
-    change_column :users, :education, :integer
+    remove_column :users, :education
+    add_column :users, :education, :integer
     add_column :users, :employment_status, :integer
   end
 end
