dfd38cfd420453b2f347287bbc0ff8ae9e9de5a8
Eoin Houlihan <ehoulih@tcd.ie>
2016-03-24 12:47:23 +0000

    Fix CORS OPTIONS requests

--- app.rb
+++ app.rb
@@ -12,6 +12,14 @@ configure do
   enable :cross_origin
 end
 
+options "*" do
+  response.headers["Allow"] = "HEAD,GET,PUT,POST,DELETE,OPTIONS"
+
+  response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept"
+
+  200
+end
+
 get '/' do
   '94% Server'
 end
