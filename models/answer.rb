17bb40b9a8d0c839324ca5708e1c2eb98e7e3806
Eoin Houlihan <ehoulih@tcd.ie>
2016-04-05 14:43:55 +0100

    Update models to cascade deletes

--- models/answer.rb
+++ models/answer.rb
@@ -1,4 +1,4 @@
 class Answer < ActiveRecord::Base
-  belongs_to :question
+  belongs_to :question, dependent: :destroy
   validates :text, presence: true, length: { maximum: 100 }
 end
--- models/question.rb
+++ models/question.rb
@@ -1,6 +1,6 @@
 class Question < ActiveRecord::Base
     has_many :answers
     has_many :entries
-    belongs_to :level
+    belongs_to :level, dependent: :destroy
     validates :text, presence: true, length: { minimum: 10, maximum: 100 }
 end
